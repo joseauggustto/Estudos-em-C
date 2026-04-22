2e3bfac16f548549daa08c08cf43c2b61cfbadb3
joseauggustto <jose.morais22aluno@gmail.com>
2026-04-22 20:25:21 -0300

    Tudo certo...

---
+++ Conceitos_Fundamentais.c
@@ -0,0 +1,119 @@
+/* Vamos começar o Estudo em C... Soluções estruturadas, ou melhor: a lógica dos algoritmos na melhor essência. 
+
+    Todo código deve ser bem estruturado para que qualquer pessoa possa ler de forma fácil. 
+
+    O primeiro passo é utilizar de boa forma a Indentação. Assim, o código fica bonito e de fácil leitura. 
+               > Isso é fundamental para organizar o código e gerar uma hierarquia de funções.
+
+    Utilizar nomes significativos é de suma importância para que as coisas fiquem de fácil entendimento.
+
+    Programar é parecido com seguir uma receita. Dividir para conquistar!
+    Dividimos um problema grande em partes menores e mais gerenciáveis, o que torna a resolução mais fácil e organizada.
+
+    Para um código limpo: Indentização, Comentários e Nomes Significativos.
+
+----------------------------
+
+   - Habilidades computacionais essênciais para um bom projeto: 
+
+          >  Decomposição: Dividir o problema inteiro em pequenas partes e resolver cada uma de forma unitária. 
+          >  Reconhecer padrões: Ao decompor, é possível notar padrões já vistos em códigos anteriores, tornando a solução mais fácil.
+          >  Abstração: Consiste em escolher priorizar os dados principais para não se atrapalhar com dados secundários pouco relevantes.
+          >  Algoritmos: São processedimentos organizados, padronizados e detalhados para resolver um problema específico. Um algoritimo é uma receita de bolo.
+
+  --------------------------
+
+  - Como a programação funciona? Vamos ver...
+
+  Programar é dar instruções precisas para o computador saber o que fazer, passo a passo sem pular nada.
+
+  A linguagem C é muito versátil, posso fazer desde Sistemas Operacionais até jogos. 
+       > Outro adendo: ela é uma linguagem copilada, ela precisa de um software para que o código possa rodar.
+
+  O passo a passo de programação: > Entendimento do problema; > Planejamento; > Codificação; > Compilação(tradução); > Teste; > Depuração(verificar erros); > Manutenção.
+
+  Linguagem de programação é um conjunto de instruções que são usadas para se comunicar com o computador, cada uma tem seu "idioma" próprio.
+  Cada linguagem tem sua própria sintaxe (regras sobre como escrever o código) e semântica (significado das instruções).
+
+  - Processo de tradução da linguagem: 
+
+  Escrevemos o código fonte na linguagem escolhida usando a extensão necessária.
+  Copilamos usando um copilador para que o código fonte se torne um arquivo que a máquina entende.
+  As vezes precisamos fazer linking, para acessar mais recursos necessários para o código funcionar.
+  E por útimo, executamos o código e vemos o que acontece. 
+ 
+  -------------------------------
+
+  Variáveis e tipos de dados: 
+
+ > Variáveis são caixas que tem nome e guardam itens com seus respectivos nomes. 
+ > Na linguagem C primeiro é definido o tipo de variável depois da-se o nome.
+ > Variáveis: Float - números menores / Double - Números gigantes.
+ > O "char" vai armazenar letras. Eu posso limitar o tamanho da palavra com [].
+
+  É muito importante dar um run na variável pra limpar o lixo da RAM, inicializando a variável vai anotar valores controlados na memória.
+
+ --------------------------------
+
+  Entrada e saída de dados:
+
+  Para saída: printf  /  Para entrada: scanf
+
+------------------------------------------------
+
+    %d: Imprime um inteiro no formato decimal.
+     
+    %i: Equivalente a %d.
+     
+    %f: Imprime um número de ponto flutuante no formato padrão.
+     
+    %e: Imprime um número de ponto flutuante na notação científica.
+     
+    %c: Imprime um único caractere.
+     
+    %s: Imprime uma cadeia (string) de caracteres.
+-----------------------------------------------
+
+    Solução Estruturada - Modularidade e Abstração. 
+
+    Modularidade é um fator muito importante na hora de criar códigos.
+    Com a modularidade eu posso criar funções que vão servir para todo o código, sem precisar refazer tudo de novo. É só criar uma função única.
+    Ela envolve escrever funções ou módulos que realizam tarefas específicas, tornando o código mais reutilizável e fácil de entender. Cada módulo deve ter uma única responsabilidade.
+
+    Abstração é me importar somente com os problemas que tem importância, e deixar as coisas superficiais um pouco de lado.
+
+
+    -------------------------------------------
+
+    Pseudocódigo e Fluxogramas. 
+    
+    O pseudocódigo é uma forma de escrever o algoritmo de forma natural, assim podemos planejar o algoritmo antes de utilizar linguagens de programação.  
+
+    Fluxograma é a sequencia lógica de um algoritmo por meio de representações gráficas. É muito bom pra aprensentar projetos. 
+
+    -------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  */
