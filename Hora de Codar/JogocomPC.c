71c720b976a1a95434ec84c5b73c2aee78086db6
joseauggustto <jose.morais22aluno@gmail.com>
2026-04-25 14:19:01 -0300

    Concluído.

--- Hora de Codar/JogocomPC.c
+++ Hora de Codar/JogocomPC.c
@@ -6,52 +6,57 @@
 
 int main (){
 
-    int numeropc, opcoes, numero;
-    char M, N, P;
+    int numeropc, numero;
+    char opcoes;
+    float M, N, I; 
 
 
-    // do{
-
-        srand(time(NULL)); // Gerador de Número Aleatório.
-        numeropc = rand() % 100 + 1; // Número de 1 até 100.
-
-
+        srand(time(NULL)); // Gerador de Número Aleatório. Ele tem que ficar fora do loop pra não prejudicar a aleatoriedade. 
+       
         printf("\n\n--- Bem Vindo ao Jogo Maior, Menor, Igual. ---\n\n");
+        
+    do{   
 
-        printf(" | Digite um número de 1 até 100.");
+        numeropc = rand() % 100 + 1; // Número de 1 até 100.
+       
+        printf(" | Digite um número de 1 até 100:  ");
         scanf("%d", &numero);
 
         printf("\n Escolha uma das opções: \n\n");
         printf(" M - Maior.\n");
         printf(" N - Menor.\n");
         printf(" I - Igual.\n");
-        printf("| 2 - Sair do Jogo.\n");
-        scanf("%d", &opcoes);
+        printf(" S - Sair do Jogo.\n");
+        scanf(" %c", &opcoes);
 
         switch (opcoes){
             case 'M':
-                printf("\n - Sua aposta foi em: MAIOR. - \n");
-                printf("Resultado: ", (numero > numeropc) ? "Você ganhou!" : "O PC ganhou!");                
-
-                break;
+            case 'm':
+                printf("\n - Sua aposta foi em: MAIOR. - \n\n");
+                printf(" Resultado: %s\n", (numero > numeropc) ? "Você ganhou!\n" : "O PC ganhou!\n");
+                printf("O número do PC foi: %d\n\n", numeropc);
+               break;
             case 'N':
-            
-
+            case 'n':
+                printf("\n - Sua aposta foi em: MENOR. - \n\n");
+                printf(" Resultado: %s\n", (numero < numeropc) ? "Você ganhou!\n" : "O PC ganhou!\n");
+                printf("O número do PC foi: %d\n\n", numeropc);
                 break;
-            case 'I':  
-            
-
+            case 'I': 
+            case 'i':
+                printf("\n - Sua aposta foi em: IGUAL. - \n\n");
+                printf(" Resultado: %s\n", (numero == numeropc) ? "Você ganhou!\n" : "O PC ganhou!\n"); 
+                printf("O número do PC foi: %d\n\n", numeropc);
                 break;
-
-
-            case 2:
-                printf("Saindo do jogo. Até logo!");
+            case 'S':
+            case 's':
+                printf("Saindo do jogo. Até logo!\n\n");
                 break;
             default:
-                printf("Opção inválida, Adeus.");
+                printf("Opção inválida.");
                 break;
         }
-    // } while (opcoes != 0);{
+    } while (opcoes != 'S' && opcoes != 's');
 }
 
 
