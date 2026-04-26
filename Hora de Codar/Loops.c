24b97f15cb9396adcc475f410db4d06854cabb29
joseauggustto <jose.morais22aluno@gmail.com>
2026-04-25 21:43:14 -0300

    Noite de Sábado

---
+++ Hora de Codar/Loops.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+// int main(){
+
+//     int num = 0;
+
+//     while (num <= 10)
+//     {
+//         printf("%d\n", num);
+//         num += 2; 
+//     }
+// return 0; 
+// }
+
+
+// #####################################
+
+// int main(){
+
+//     int i = 0;
+
+//     while (i <= 10)
+//     {
+//         if (i % 2 == 0){
+//             printf(" %d ", i);
+//         }
+
+//     i++;
+
+//     }
+
+// return 0;
+
+// }
+
+// #####################################
+
+// int main(){
+
+//     int numero;
+
+//     do
+//     {
+//         printf("Digite um número par para sair.  ");
+//         scanf("%d", &numero);
+
+//         if (numero % 2 == 0)
+//         {
+//             printf("É par.\n");
+//         } else {
+//             printf("É impar.\n");
+//         }
+        
+        
+//     } while (numero % 2 != 0);
+    
+// return 0;
+
+// }
+
+// #####################################
+
+// int main(){
+
+//     int numero, i; 
+
+//     printf("Digite um número para a tabuada. ");
+//     scanf("%d", &numero);
+
+//     for (i = 0; i <= 10; i++)
+//     {
+//         printf("%d x %d = %d\n", numero, i, numero*i);
+   
+//     }
+    
+// return 0; 
+// }
